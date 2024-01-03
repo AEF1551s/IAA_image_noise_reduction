fcdeeffdb7a7a0b76d2d523a200a2d728ca0d015
Krišjānis Ivbulis <65708516+AEF1551s@users.noreply.github.com>
2024-01-03 18:51:26 +0200

    Add NL_Means class, comments, places for other algorithms (#9)
    * Add show_groupbox helper method
    
    * Add show_groupbox summary
    
    * Create NL_Means class
    
    Added empty holder methods and attributes.
    
    * Declare NL_Means, add comments
    
    * Add TODO

--- Form1.cs
+++ Form1.cs
@@ -6,7 +6,7 @@ namespace IAA_image_noise_reduction
     public partial class Form1 : Form
     {
         private ImageClass ImageClass = new ImageClass();
-
+        
         public Form1()
         {
             InitializeComponent();
@@ -26,10 +26,47 @@ namespace IAA_image_noise_reduction
         }
 
         //Algoritmu parametru attēlošana
+        //Helper metode
+
+        /// <summary>
+        /// Displays the specified group box based on the given parameter representing different image processing techniques or filters.
+        /// </summary>
+        /// <param name="box">An integer value representing the type of group box to be displayed:
+        /// 0 - NL Means,
+        /// 1 - TV Regularization,
+        /// 2 - Gaussian Blur,
+        /// 3 - Weiner Filter.</param>
+        private void show_groupbox(int box)
+        {
+            switch (box)
+            {
+                case 0:
+                    nl_group_box.Visible = true;
+                    //ALL other groupbox.Visible = false;
+                    break;
+                case 1:
+                    //tv reg box.Visible = true;
+                    //ALL other groupbox.Visible = false;
+                    break;
+                case 2:
+                    //Gaussianblur_box.Visible = true;
+                    //ALL other groupbox.Visible = false;
+                    break;
+                case 3:
+                    //Weiner filter.Visible = true;
+                    //ALL other groupbox.Visible = false;
+                    break;
+                default:
+                    break;
+
+            }
+            return;
+        }
         //NL Means
+        //TODO: Add check (arrow and cross) for valid chosen parameter values
         private void button_nlmeans_Click(object sender, EventArgs e)
         {
-            nl_group_box.Visible = true;
+            show_groupbox(0);
             button_denoise_apply.Enabled = true;
         }
 
