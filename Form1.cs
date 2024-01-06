1f5068f47b5e4ac27b3e625cebd5c272767ec3d2
Krišjānis Ivbulis <65708516+AEF1551s@users.noreply.github.com>
2024-01-06 17:26:33 +0200

    add multiple gorupbox, helper functions.
    Add all algorithm grouboxes, helper functions for displaying groupboxes, checking for valid parameters.

--- Form1.cs
+++ Form1.cs
@@ -10,6 +10,13 @@ namespace IAA_image_noise_reduction
         public Form1()
         {
             InitializeComponent();
+
+            //Sets all groupbox to top of each other
+            Point groupbox_locations = new Point(955, 185);
+            nl_groupbox.Location = groupbox_locations;
+            tvreg_groupbox.Location = groupbox_locations;   
+            gblur_groupbox.Location = groupbox_locations;
+            wfilter_groupbox.Location = groupbox_locations;
         }
 
         //Menu strip functions
@@ -35,26 +42,27 @@ namespace IAA_image_noise_reduction
         /// 0 - NL Means,
         /// 1 - TV Regularization,
         /// 2 - Gaussian Blur,
-        /// 3 - Weiner Filter.</param>
+        /// 3 - Weiner Filter. </param>
         private void show_groupbox(int box)
         {
+            nl_groupbox.Visible = false;
+            tvreg_groupbox.Visible = false;
+            gblur_groupbox.Visible = false;
+            wfilter_groupbox.Visible = false;
+
             switch (box)
             {
                 case 0:
-                    nl_group_box.Visible = true;
-                    //ALL other groupbox.Visible = false;
+                    nl_groupbox.Visible = true;
                     break;
                 case 1:
-                    //tv reg box.Visible = true;
-                    //ALL other groupbox.Visible = false;
+                    tvreg_groupbox.Visible = true;
                     break;
                 case 2:
-                    //Gaussianblur_box.Visible = true;
-                    //ALL other groupbox.Visible = false;
+                    gblur_groupbox.Visible = true;
                     break;
                 case 3:
-                    //Weiner filter.Visible = true;
-                    //ALL other groupbox.Visible = false;
+                    wfilter_groupbox.Visible = true;
                     break;
                 default:
                     break;
@@ -62,23 +70,70 @@ namespace IAA_image_noise_reduction
             }
             return;
         }
+
+        //Helper function, to enable Apply button, when all algorithm parameters are valid
+        private bool enable_apply_button(int mode)
+        {
+            bool enable = false;
+
+            //Check if picture is loaded
+            if (pictureBox2.Image == null)
+            { 
+                return false; 
+            }
+            //Check if all mode parameters are valid
+            //TODO: Fill all algorithm paramter checks
+            switch (mode)
+            {
+                case 0:
+                    enable = true;
+                    break;
+                case 1:
+                    enable = true;
+                    break;
+                case 2:
+                    enable = true;
+                    break;
+                case 3:
+                    enable = true;
+                    break;
+                default:
+                    break;
+
+            }
+
+            return enable;
+        }
         //NL Means
         //TODO: Add check (arrow and cross) for valid chosen parameter values
         private void button_nlmeans_Click(object sender, EventArgs e)
         {
+
             show_groupbox(0);
-            button_denoise_apply.Enabled = true;
+            button_denoise_apply.Enabled = enable_apply_button(0);
         }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
         //Total variation regularization
+        private void button_tvreg_Click(object sender, EventArgs e)
+        {
+            show_groupbox(1);
+            button_denoise_apply.Enabled = enable_apply_button(1);
+        }
 
         //Gaussian blur
+        private void button_gblur_Click(object sender, EventArgs e)
+        {
+            show_groupbox(2);
+            button_denoise_apply.Enabled = enable_apply_button(2);
+        }
 
         //Wiener Filtering
+        private void button_wfilter_Click(object sender, EventArgs e)
+        {
+            show_groupbox(3);
+            button_denoise_apply.Enabled = enable_apply_button(3);
+        }
+
+
+
     }
 }
